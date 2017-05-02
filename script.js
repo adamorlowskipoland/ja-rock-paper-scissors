5e828b95493f7d3d3e12099d817dfa3acb724353
Adam Orlowski <adamorlowskipoland@icloud.com>
2017-05-02 23:29:28 +0200

    add playerSelection

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const model = {
     "computerChoose" : document.getElementById('computerChoose'),
     "playerChoose" : document.getElementById('playerChoose'),
 
+    "computerScore" : document.getElementById('computerScore'),
+    "playerScore" : document.getElementById('playerScore'),
+
     "options" : {
         "rock" : "fa fa-hand-rock-o",
         "paper" : "fa fa-hand-paper-o",
@@ -12,35 +15,53 @@ const model = {
 }
 
 const operator = {
-    "getSelection" : function(domElement) {
+    "computerPoints" : 0,
+    "playerPoints" : 0,
+    "computerChoice" : "",
+    "playerChoice" : "",
+
+    "getComputerSelection" : function(domElement) {
         var pick = Math.random();
         if (pick < 0.67 && pick > 0.33) {
+            this.computerChoice = "scissors";
             domElement.className = model.options.scissors;
         } else if (pick <= 0.33) {
+            this.computerChoice = "rock";
             domElement.className = model.options.rock;
         } else {
+            this.computerChoice = "paper";
             domElement.className = model.options.paper;
         }
     },
+    "getPlayerSelection" : function() {
+        if (model.playerChooseSelect.value === "scissors") {
+            this.playerChoice = "scissors";
+        } else if (model.playerChooseSelect.value === "rock") {
+            this.playerChoice = "rock";            
+        } else {
+            this.playerChoice = "paper";            
+        }
+        console.log(this.playerChoice);
+    },
     "setSelections" : function() {
         model.playerChoose.className = model.options[model.playerChooseSelect.value];
-        this.getSelection(model.computerChoose);
+        this.getPlayerSelection();
+        this.getComputerSelection(model.computerChoose);
+    },
+    "compareSelections" : function() {
+        // if ()
     },
     "eventListeners" : function() {
         model.playBtn.addEventListener('click', function() {
             operator.setSelections();
-            // view.initDisplay();
         });
     }
 }
 
 const view = {
     "initDisplay" : function() {
-        // model.playerChoose.className = model.options[model.playerChooseSelect.value];
-        // console.log(model.playerChooseSelect.value);
-        // console.log(model.playerChoose);
+        operator.eventListeners();
     }
 }
 
-operator.eventListeners();
-// view.initDisplay();
+view.initDisplay();
